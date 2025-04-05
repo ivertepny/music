ba49ed90601bc22eb5cafe81a93b3c9c2607f941
Igor Vertepny <ivertepny@gmail.com>
2025-04-05 22:36:38 +0300

    start with users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ['email', 'username', 'is_staff']
+    search_fields = ['email', 'username']
+    ordering = ['email']
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (None, {'fields': ('email',)}),
+    )
+
+admin.site.register(User, UserAdmin)
